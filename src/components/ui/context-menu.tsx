fd1287dfbd80d563724975c3a0beaf1cf572fe17
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-11-11 12:56:05 +0530

    context trigger

--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -5,7 +5,6 @@ import { cn } from "@/lib/utils";
 
 const ContextMenu = ContextMenuPrimitive.Root;
 const ContextMenuTrigger = ContextMenuPrimitive.Trigger;
-
 const ContextMenuGroup = ContextMenuPrimitive.Group;
 
 const ContextMenuPortal = ContextMenuPrimitive.Portal;
